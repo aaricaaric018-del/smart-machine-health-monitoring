568f4fdd588547cbb80ce91c3aa5d87d35888571
aaricaaric018-del <aaricaaric018@gmail.com>
2026-07-18 16:57:26 +0530

    Added prediction API

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
@@ -16,4 +16,28 @@ def machine_data():
         "vibration": 0.45,
         "rpm": 1450,
         "status": "Healthy"
+    }
+
+
+class SensorData(BaseModel):
+    temperature: float
+    vibration: float
+    rpm: int
+
+@app.post("/predict")
+def predict(data: SensorData):
+
+    if data.temperature > 90:
+        return {
+            "health_score": 60,
+            "failure_probability": 0.75,
+            "maintenance_required": True,
+            "recommendation": "Inspect machine immediately"
+        }
+
+    return {
+        "health_score": 95,
+        "failure_probability": 0.05,
+        "maintenance_required": False,
+        "recommendation": "Machine operating normally"
     }
